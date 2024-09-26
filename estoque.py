d7447d0b5e946b4bb1c479932983fb7772096102
Ackin.Dev <ackinlino123@gmail.com>
2024-09-26 20:45:13 -0300

    Add: Função para deletar o produto

--- estoque.py
+++ estoque.py
@@ -56,6 +56,12 @@ class GerenciamentoProduto:
         cursor = conn.cursor()
         cursor.execute("UPDATE estoque SET preco = ?WHERE id = ?", (preco, id))
         conn.commit()
+    
+    def deletar_produto(self, id):
+        conn = sqlite3.connect('estoque.db')
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM estoque WHERE id = ?", (id,))
+        conn.commit()          
         
 print("GERENCIAMENTO DE ESTOQUE")
 
@@ -81,13 +87,16 @@ while True:
     
     opcao = int(input("Digite o número das opções acima: "))
     
+    # Cadastrar o produto para adicionar para o banco de dados
     if opcao == 1:
         print("""
               Cadastre o seu produto!!! 
               Insira o id, nome, categoria, quantidade, preco e a localização!!!
         """)
         nome = str(input("Nome do produto: ")).upper()
-        categoria = str(input("Categoria do produto: ")).upper()
+        categoria = str(input("""
+        Escolha uma das opções de categoria do produto;\n
+        Televisão, Console de Jogos, Leitores de Mídia, Tablet, Smartphone, Sistemas de Áudio, Pen Drives e Cartões de Memória\nEscolha: """)).upper()        
         marca = str(input("Digite a marca do produto: ")).upper()     
         quantidade = int(input("Digite a quantidade do produto: "))
         preco = float(input("Digite o preço do produto: "))
@@ -96,26 +105,31 @@ while True:
         local = f"Corredor: {corredor.upper()} Rua: {rua} "
         produto = GerenciamentoProduto(id,nome,categoria,marca,quantidade,preco,local)
         produto.add_produto((nome,categoria,marca,quantidade,preco,local))        
-        
+    
+    #Opção para exibir a lista do banco de dados por pandas    
     elif opcao == 2:  
         estoque = GerenciamentoProduto(id, nome, categoria,marca,quantidade, preco, local)
         estoque.lista_produto()
         
+    #Opção para atualizar o preço do produto pelo id    
     elif opcao == 3:
         print("Atualizar o preço do produto")
         estoque = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
         estoque.lista_produto()
-        
         id = int(input("Digite o id do produto: "))
         preco = float(input("Digite o novo preço do produto: "))
         produto = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
         produto.atualizar_produto(id,preco)
         print("Preço atualizado com sucesso!!!")
-        
-        
-            
+              
+    #Opção para remover um produto do banco de dados        
     elif opcao == 4:
         print("Escolha um produto para remover: ")
+        estoque = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
+        estoque.lista_produto()
+        id = int(input("Escolha um produto pelo id para remover do banco de dados: "))
+        estoque.deletar_produto(id)
+        
     elif opcao == 5:
         print("Relatório do produto")
     elif opcao == 6:
