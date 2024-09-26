a37e01afd67cb0e2a532c5efe382a22c571251da
Ackin.Dev <ackinlino123@gmail.com>
2024-09-25 23:19:42 -0300

    Reorganizando o banco de dados

--- estoque.py
+++ estoque.py
@@ -12,6 +12,7 @@ CREATE TABLE IF NOT EXISTS estoque
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
     categoria TEXT NOT NULL,
+    marca TEXT NOT NULL,
     quantidade INTEGER NOT NULL,
     preco REAL NOT NULL,
     localizacao TEXT NOT NULL
@@ -24,10 +25,11 @@ conn.commit()
 
 #Classe dos Produtos que serão alterados, inseridos, adicionados ou removidos 
 class GerenciamentoProduto:
-    def __init__(self,id,nome,categoria,quantidade,preco,localizacao):
+    def __init__(self,id,nome,categoria,marca,quantidade,preco,localizacao):
         self.id = id
         self.nome = nome
         self.categoria = categoria
+        self.marca = marca
         self.quantidade = quantidade
         self.preco = preco
         self.localizacao = localizacao
@@ -35,7 +37,7 @@ class GerenciamentoProduto:
     #função para inserir um produto  no banco de dados
     def add_produto(self, produto):
         cursor.execute("""
-        INSERT INTO estoque(nome, categoria, quantidade, preco, localizacao)VALUES (?, ?, ?, ?, ?);
+        INSERT INTO estoque(nome, categoria, marca, quantidade, preco, localizacao)VALUES (?, ?, ?, ?, ?, ?);
         """, produto)
         conn.commit()
 
@@ -45,7 +47,7 @@ class GerenciamentoProduto:
         read = "SELECT * FROM estoque"
         cursor.execute(read)
         produtos = cursor.fetchall()
-        df = pd.DataFrame(produtos, columns=["id","NOME",  "CATEGORIA", "QUANTIDADE", "PREÇO", "LOCALIZAÇÃO"])
+        df = pd.DataFrame(produtos, columns=["id","NOME","CATEGORIA","MARCA","QUANTIDADE","PREÇO", "LOCALIZAÇÃO"])
         print(df.head())
             
     # Função para atualizar um produto no banco de dados
@@ -60,6 +62,7 @@ print("GERENCIAMENTO DE ESTOQUE")
 #Variáveis de inicialização para as funções das classes
 id = None
 nome = ""
+marca = ""
 categoria = ""
 quantidade = 0
 preco = 0.0
@@ -84,28 +87,30 @@ while True:
               Insira o id, nome, categoria, quantidade, preco e a localização!!!
         """)
         nome = str(input("Nome do produto: ")).upper()
-        categoria = str(input("Categoria do produto: ")).upper()     
+        categoria = str(input("Categoria do produto: ")).upper()
+        marca = str(input("Digite a marca do produto: ")).upper()     
         quantidade = int(input("Digite a quantidade do produto: "))
         preco = float(input("Digite o preço do produto: "))
         corredor = str(input("Localização do produto, Corredor: "))
         rua = int(input("Localização do produto, Rua: "))
         local = f"Corredor: {corredor.upper()} Rua: {rua} "
-        produto = GerenciamentoProduto(id,nome,categoria,quantidade,preco,local)
-        produto.add_produto((nome,categoria,quantidade,preco,local))        
+        produto = GerenciamentoProduto(id,nome,categoria,marca,quantidade,preco,local)
+        produto.add_produto((nome,categoria,marca,quantidade,preco,local))        
         
     elif opcao == 2:  
-        estoque = GerenciamentoProduto(id, nome, categoria, quantidade, preco, local)
+        estoque = GerenciamentoProduto(id, nome, categoria,marca,quantidade, preco, local)
         estoque.lista_produto()
         
     elif opcao == 3:
         print("Atualizar o preço do produto")
-        estoque = GerenciamentoProduto(id, nome, categoria, quantidade, preco, local)
+        estoque = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
         estoque.lista_produto()
         
         id = int(input("Digite o id do produto: "))
         preco = float(input("Digite o novo preço do produto: "))
-        produto = GerenciamentoProduto(id, nome, categoria, quantidade, preco, local)
+        produto = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
         produto.atualizar_produto(id,preco)
+        print("Preço atualizado com sucesso!!!")
         
         
             
@@ -115,7 +120,6 @@ while True:
         print("Relatório do produto")
     elif opcao == 6:
         print("Saindo do programa...")
-        os.system('clear')
         break
     else:
         print("Opção invalida, tente novamente!")
