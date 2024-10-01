491fa734d5236aded4eaed9d17ef7fef1ffccd4a
Ackin.Dev <ackinlino123@gmail.com>
2024-10-01 18:33:04 -0300

    Add: reajuste de atualizar produtos, função para vender produtos.

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# Requisitos para esta aplicação #
+# Requisitos para esta aplicação
 
 # No banco de dados deve incluir id, nome, categoria, quantidade, preço e localização
 
 # Temática = Produtos Eletrônicos
 
-# Categorias = Memória, Monitor,  Teclado, Mouse, Placa de Vídeo, Placa Mãe, Processador, Headset
+# Categorias = Memória, Monitor, Teclado, Mouse, Placa de Vídeo, Placa Mãe, Processador, Headset
 
 Este projeto faz parte do Projeto Inovação da Faculdade, a seguir este é o meu portfólio em Python.
 Será o sistema de gerenciamento de Estoque.
 
 - [x] • Cadastro de Produtos: O sistema deve permitir o cadastro de novos produtos, incluindo
-informações como nome, categoria, quantidade em estoque, preço e localização no depósito.
+      informações como nome, categoria, quantidade em estoque, preço e localização no depósito.
 
 - [x] • Atualização de Estoque: O sistema deve permitir a atualização da quantidade de produtos
-em estoque quando novos itens são recebidos ou quando produtos são vendidos.
+      em estoque quando novos itens são recebidos ou quando produtos são vendidos.
 
 • Rastreamento de Localização: O sistema deve possibilitar o rastreamento da localização dos
 produtos dentro dos depósitos para facilitar a logística.
@@ -21,14 +21,8 @@ produtos dentro dos depósitos para facilitar a logística.
 • Relatórios: O sistema deve gerar relatórios sobre o estado do estoque, destacando produtos
 com estoque baixo, excesso de estoque e movimentação de produtos.
 
-• Tabela Verdade: Deve-se criar uma tabela verdade para definir as combinações de valores
-(True e False) para as variáveis booleanas da situação problema.
-
-• Diagrama de Caso de Uso: Crie um Diagrama de Casos de Uso para o sistema de
-gerenciamento de estoque da empresa.
-
 - [x] • Definição de Estruturas de Dados: Estruturas bem definidas para produtos, categorias e
-movimentações.
+      movimentações.
 
 - [x] • Algoritmos de Cadastro e Consulta: Funções para cadastrar e consultar dados no sistema.
 
@@ -37,5 +31,3 @@ atualizar o estoque.
 
 • Relatórios e Consultas: Funções para gerar relatórios e consultar o histórico de
 movimentações.
-
-
--- estoque.py
+++ estoque.py
@@ -42,6 +42,7 @@ class GerenciamentoProduto:
         """, produto)
         conn.commit()
 
+    #Exibe a tabela do produto do banco de dados
     def lista_produto(self):
         conn = sqlite3.connect('estoque.db')
         cursor = conn.cursor()
@@ -49,21 +50,31 @@ class GerenciamentoProduto:
         cursor.execute(read)
         produtos = cursor.fetchall()
         df = pd.DataFrame(produtos, columns=["id","NOME","CATEGORIA","MARCA","QUANTIDADE","PREÇO", "LOCALIZAÇÃO", "DATA DE CADASTRO"])
-        print(df.head())
+        print(df)
             
     # Função para atualizar um produto no banco de dados
-    def atualizar_produto(self,id,preco):
+    def atualizar_produto(self,id,quantidade):
         conn = sqlite3.connect('estoque.db')
         cursor = conn.cursor()
-        cursor.execute("UPDATE estoque SET preco = ?WHERE id = ?", (preco, id))
+        cursor.execute("UPDATE estoque SET quantidade = quantidade + ? WHERE id = ?", (quantidade, id))
         conn.commit()
     
+    # Função para vender uma quantia específica de produtos baseados no id e quantidade
+    def vender_produto(self, id, quantidade):
+        conn = sqlite3.connect('estoque.db')
+        cursor = conn.cursor()
+        cursor.execute("UPDATE estoque SET quantidade = quantidade - ? WHERE Id = ?", (quantidade, id)) 
+        conn.commit()
+        
+    # Função da classe que deleta um produto do banco de dados
     def deletar_produto(self, id):
         conn = sqlite3.connect('estoque.db')
         cursor = conn.cursor()
         cursor.execute("DELETE FROM estoque WHERE id = ?", (id,))
         conn.commit()       
-       
+    
+    
+    
 print("GERENCIAMENTO DE ESTOQUE")
 
 #Variáveis de inicialização para as funções das classes
@@ -75,29 +86,47 @@ quantidade = 0
 preco = 0.0
 local = ""
 
-print("""\n
-    1.Inserir Produtos
-    2.Lista dos Produtos
-    3.Atualizar Produto
-    4.Deletar Produto
-    5.Relatório do Produto
-    6.Sair    
-    """)   
-
 while True:
     
-    opcao = int(input("Digite o número das opções acima: "))
+    opcao = int(input("""\n
+            1.Inserir Produtos
+            2.Lista dos Produtos
+            3.Atualizar Produto
+            4.Deletar Produto
+            5.Relatório do Produto
+            6.Localização
+            7.Vender
+            8.Sair   
+            \nDigite o número das opções acima: """))
     
     # Cadastrar o produto para adicionar para o banco de dados
     if opcao == 1:
         print("""
               Cadastre o seu produto!!! 
-              Insira o id, nome, categoria, quantidade, preco e a localização!!!
+              Insira o nome
+              categoria
+              quantidade
+              preco 
+              e a localização!!!
         """)
         nome = str(input("Nome do produto: ")).upper()
         categoria = str(input("""
-        Escolha uma das opções de categoria do produto;\n
-        Televisão, Console de Jogos, Leitores de Mídia, Tablet, Smartphone, Sistemas de Áudio, Pen Drives e Cartões de Memória\nEscolha: """)).upper()        
+        Escolha uma das opções de categoria do produto:
+        Televisão
+        Console de Jogos 
+        Leitores de Mídia
+        Tablet, Smartphone
+        Sistemas de Áudio 
+        Pen Drives
+        Cartões de Memória
+        Monitor 
+        Teclado
+        Mouse
+        Placa de Vídeo 
+        Placa Mãe
+        Processador 
+        Headset
+        Escolha: """)).upper()
         marca = str(input("Digite a marca do produto: ")).upper()     
         quantidade = int(input("Digite a quantidade do produto: "))
         preco = float(input("Digite o preço do produto: "))
@@ -111,6 +140,12 @@ while True:
     elif opcao == 2:  
         estoque = GerenciamentoProduto(id, nome, categoria,marca,quantidade, preco, local)
         estoque.lista_produto()
+        voltar = input("\nDeseja voltar para o menu principal?: (sim/não) ")
+        if voltar == "sim":
+            continue
+        else:
+            os.system('clear') or None
+            break       
         
     #Opção para atualizar o preço do produto pelo id    
     elif opcao == 3:
@@ -118,11 +153,10 @@ while True:
         estoque = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
         estoque.lista_produto()
         id = int(input("Digite o id do produto: "))
-        preco = float(input("Digite o novo preço do produto: "))
-        produto = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local).date.today()
-        produto.atualizar_produto(id,preco)
-        print("Preço atualizado com sucesso!!!")
-              
+        quantidade = float(input("Quantidade novo do produto: "))
+        produto = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
+        produto.atualizar_produto(id,quantidade)
+                      
     #Opção para remover um produto do banco de dados        
     elif opcao == 4:
         print("Escolha um produto para remover: ")
@@ -133,7 +167,25 @@ while True:
         
     elif opcao == 5:
         print("Relatório do produto")
+        voltar = input("\nDeseja voltar para o menu principal?: (sim/não) ")
+        if voltar == "sim":
+            continue
+        else:
+            break
+    
     elif opcao == 6:
+        print("Localização do produto")
+        
+    elif opcao == 7:
+        print("Vender um produto")
+        estoque = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
+        estoque.lista_produto()
+        id = int(input("Digite o id do produto: "))
+        quantidade = float(input("Quanto irá ser vendido: "))
+        produto = GerenciamentoProduto(id, nome, categoria,marca, quantidade, preco, local)
+        produto.vender_produto(id,quantidade)
+                
+    elif opcao == 8:
         print("Saindo do programa...")
         os.system('clear') or None
         break
